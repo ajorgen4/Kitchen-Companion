9b80ee69de49030224bd4140d4eec2f71676ed90
ajorgen3 <113084414+ajorgen3@users.noreply.github.com>
2024-03-19 00:41:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kitchencompanion/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.kitchencompanion;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationBarView bottomNavigationView;
+
+    private final Map<Integer, Fragment> fragmentMap = new HashMap<>();
+
+    // Nav bar code adapted from: https://www.geeksforgeeks.org/bottom-navigation-bar-in-android/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        fragmentMap.put(R.id.recipes, new Tab1());
+        fragmentMap.put(R.id.pantry, new Tab2());
+        fragmentMap.put(R.id.shopping, new Tab3());
+        fragmentMap.put(R.id.settings, new Tab4());
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (fragmentMap.containsKey(itemId)) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.flFragment, fragmentMap.get(itemId))
+                            .commitNow();
+                    return true;
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.recipes);
+    }
+}
---
+++ app/src/main/java/com/example/kitchencompanion/Tab4.java
@@ -0,0 +1,20 @@
+package com.example.kitchencompanion;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Tab4 extends Fragment {
+    public Tab4(){
+        // require a empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_tab4, container, false);
+    }
+}
