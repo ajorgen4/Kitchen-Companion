52f9d94b45d5e77901489b4b13d3521a4b5a9510
azong4406 <a.zong5.1@gmail.com>
2024-04-09 18:03:34 -0400

    Finished All functionality.(Search Bar not implemented)

--- app/src/main/java/com/example/kitchencompanion/Tab3.java
+++ app/src/main/java/com/example/kitchencompanion/Tab3.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,10 +24,9 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.time.LocalDate;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -44,16 +44,25 @@ public class Tab3 extends Fragment {
 
     // Used in FoodType selector
     private ArrayList<FoodType> foodSelectorList;
+    private HashMap<String, List<ShopListItem>> ShopListPresets;
     private FoodTypeSelectorAdapter foodSelectorListAdapter;
     private Dialog foodTypeSelector;
     // THIS IS WHERE YOU ACCESS THE FOOD SELECTED BY THE FOODTYPE SELECTOR
     private FoodType selectedFood;
+    private String selectedKey = "";
 
     private boolean shoppingMode;
     public Tab3(HashMap<Integer, FoodType> foodDictionary, Tab2 tab2){
         this.foodDictionary = foodDictionary;
 
         shopList = new ArrayList<ShopListItem>();
+        ShopListPresets = new HashMap<String, List<ShopListItem>>();
+        shopList.add(new ShopListItem(foodDictionary.get(1),5));
+        shopList.add(new ShopListItem(foodDictionary.get(2),3));
+        shopList.add(new ShopListItem(foodDictionary.get(3),1));
+        ArrayList<ShopListItem> test = new ArrayList<ShopListItem>();
+        test.add(new ShopListItem(foodDictionary.get(2),10));
+        ShopListPresets.put("10 Oranges", test);
         shoppingMode = false;
         this.tab2 = tab2;
     }
@@ -68,6 +77,8 @@ public class Tab3 extends Fragment {
         Button submitShop = view.findViewById(R.id.submitListButton);
         Button shopMode = view.findViewById(R.id.buyModeButton);
         FloatingActionButton addButton = view.findViewById(R.id.addShopListFoodButton);
+        FloatingActionButton addPreset = view.findViewById(R.id.addPresetShopListButton);
+        FloatingActionButton pushPreset = view.findViewById(R.id.pushPresetShopListButton);
         if (shoppingMode) {
             shopMode.setText("Edit List");
             submitShop.setVisibility(View.VISIBLE);
@@ -132,11 +143,12 @@ public class Tab3 extends Fragment {
                 }
             }
         });
+        /*CODE FOR ADDING ITEMS TO THE SHOPPING LIST*/
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.add_shoplist_dialog, null);
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.push_shoplist_dialog, null);
 
                 // Find and set up the views inside the dialog layout
                 // ...
@@ -307,10 +319,118 @@ public class Tab3 extends Fragment {
                 addButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        /*adapter.addShopListItem(new ShopListItem(,amount));*/
+                        adapter.addShopListItem(new ShopListItem(selectedFood,Integer.parseInt(amount.getText().toString())));
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+        /*END OF CODE FOR THE ADD BUTTON*/
+        /*Code for adding presets to the library of presets*/
+        addPreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.add_shoplist_preset_dialog, null);
+                // Find and set up the views inside the dialog layout
+                // ...
+                EditText listName = dialogView.findViewById(R.id.shopListAddPresetNameInput);
+                Button presetAdd = dialogView.findViewById(R.id.shopListAddPresetCreateButton);
+                Button presetCancel = dialogView.findViewById(R.id.shopListAddPresetCancelButton);
+                builder.setView(dialogView);
+
+                AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+                // Set the dialog to occupy approximately 75% of the screen
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int width = (int) (displayMetrics.widthPixels * 0.75);
+                int height = (int) (displayMetrics.heightPixels * 0.75);
+                dialog.getWindow().setLayout(width, height);
+
+                // Dim the background
+                dialog.getWindow().setDimAmount(0.5f);
+                presetAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ArrayList<ShopListItem> temp = new ArrayList<ShopListItem>();
+                        for(ShopListItem i : shopList){
+                            temp.add(new ShopListItem(i.getFood(),i.getAmount()));
+                        }
+                        ShopListPresets.put(listName.getText().toString(), temp);
+                        dialog.dismiss();
+                    }
+                });
+                presetCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+        /*Code for spinner adapted from https://developer.android.com/develop/ui/views/components/spinner#java*/
+        pushPreset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.shoplist_preset_dialog, null);
+
+                // Find and set up the views inside the dialog layout
+                // ...
+                Button presetCancel = dialogView.findViewById(R.id.shopListPushPresetCancelButton);
+                Button presetPush = dialogView.findViewById(R.id.shopListPushPresetCreateButton);
+                Spinner presetList = dialogView.findViewById(R.id.shopListPushPresetSpinner);
+                List<String> presetListKeys= new ArrayList<String>(ShopListPresets.keySet());
+                ArrayAdapter<String> shopListAdapter = new ArrayAdapter<>(getContext(), R.layout.shoplist_spinner, presetListKeys);
+
+                presetList.setAdapter(shopListAdapter);
+
+                builder.setView(dialogView);
+
+                AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+                // Set the dialog to occupy approximately 75% of the screen
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int width = (int) (displayMetrics.widthPixels * 0.75);
+                int height = (int) (displayMetrics.heightPixels * 0.75);
+                dialog.getWindow().setLayout(width, height);
+
+                // Dim the background
+                dialog.getWindow().setDimAmount(0.5f);
+
+                presetList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        selectedKey = parent.getItemAtPosition(position).toString();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        selectedKey = "";
+                    }
+                });
+                presetPush.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!selectedKey.isEmpty()) {
+                            adapter.addShopListItemBatch(ShopListPresets.get(selectedKey));
+                            dialog.dismiss();
+                        }
                     }
                 });
 
+                presetCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
                 dialog.show();
             }
         });
