26b6542b9071ab006c3501ae8933902e656dd2ba
azong4406 <a.zong5.1@gmail.com>
2024-05-06 21:58:40 -0400

    Changed Preset Dialog

--- app/src/main/java/com/example/kitchencompanion/Tab3.java
+++ app/src/main/java/com/example/kitchencompanion/Tab3.java
@@ -376,7 +376,13 @@ public class Tab3 extends Fragment {
                 EditText listName = dialogView.findViewById(R.id.shopListAddPresetNameInput);
                 Button presetAdd = dialogView.findViewById(R.id.shopListAddPresetCreateButton);
                 Button presetCancel = dialogView.findViewById(R.id.shopListAddPresetCancelButton);
+                ListView addPresetList = dialogView.findViewById(R.id.shopListAddPresetListView);
+                Button presetAddFood = dialogView.findViewById(R.id.shopListAddPresetAddFoodButton);
+                List<ShopListItem> presetList = new ArrayList<ShopListItem>();
                 builder.setView(dialogView);
+
+                ShopListAdapter presetAddAdapter = new ShopListAdapter(getContext(), presetList, false);
+                addPresetList.setAdapter(presetAddAdapter);
 
                 AlertDialog dialog = builder.create();
                 dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
@@ -390,6 +396,218 @@ public class Tab3 extends Fragment {
 
                 // Dim the background
                 dialog.getWindow().setDimAmount(0.5f);
+
+                presetAddFood.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        View addDialogView = LayoutInflater.from(getContext()).inflate(R.layout.push_shoplist_dialog, null);
+
+                        // Find and set up the views inside the dialog layout
+                        // ...
+                        Button cancelButton = addDialogView.findViewById(R.id.shopListAddFoodCancelButton);
+                        Button addButton = addDialogView.findViewById(R.id.shopListAddFoodCreateButton);
+                        EditText amount = addDialogView.findViewById(R.id.shopListAddFoodCountInput);
+
+                /*
+                    ########################################################
+                    ######## FOODTYPE SELECTOR CODE, DO NOT CHANGE! ########
+                    ########################################################
+                 */
+                        final TextView foodTypeSelector = addDialogView.findViewById(R.id.shopListAddFoodFoodTypeSelector);
+
+                        // Code adapted from: https://www.youtube.com/watch?v=5iIXg4-Iw3U
+                        // This initialization must stay here to ensure it remains up to date with foodDictionary
+                        foodSelectorList = new ArrayList<FoodType>(foodDictionary.values());
+                        foodTypeSelector.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Tab3.this.foodTypeSelector = new Dialog(requireContext());
+                                Tab3.this.foodTypeSelector.setContentView(R.layout.food_type_selector);
+                                Tab3.this.foodTypeSelector.getWindow().setLayout(650, 800);
+                                Tab3.this.foodTypeSelector.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                                Tab3.this.foodTypeSelector.show();
+
+                                EditText foodSelectorSearchBar = Tab3.this.foodTypeSelector.findViewById(R.id.foodSelectorSearchBar);
+                                ListView foodSelectorListView = Tab3.this.foodTypeSelector.findViewById(R.id.foodSelectorListView);
+
+                                foodSelectorListAdapter = new FoodTypeSelectorAdapter(getContext(), foodSelectorList);
+                                foodSelectorListView.setAdapter(foodSelectorListAdapter);
+
+                                foodSelectorSearchBar.addTextChangedListener(new TextWatcher() {
+                                    @Override
+                                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                                    }
+
+                                    @Override
+                                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                        foodSelectorListAdapter.getFilter().filter(s);
+                                    }
+
+                                    @Override
+                                    public void afterTextChanged(Editable s) {
+
+                                    }
+                                });
+
+                                foodSelectorListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                        // Pass the selected food
+                                        selectedFood = foodSelectorListAdapter.getItem(position);
+                                        // Update visuals
+                                        foodTypeSelector.setText(selectedFood.getItemName());
+                                        // Dismiss dialog
+                                        Tab3.this.foodTypeSelector.dismiss();
+                                    }
+                                });
+
+                                ImageButton foodSelectorExitButton = Tab3.this.foodTypeSelector.findViewById(R.id.foodSelectorCloseButton);
+                                foodSelectorExitButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Tab3.this.foodTypeSelector.dismiss();
+                                    }
+                                });
+
+                                // FOODTYPE SELECTOR ADD BUTTON
+                                Button foodSelectorAddButton = Tab3.this.foodTypeSelector.findViewById(R.id.foodSelectorAddButton);
+                                foodSelectorAddButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Dialog dialog = new Dialog(requireContext());
+                                        dialog.setContentView(R.layout.add_foodtype_dialog);
+
+                                        Button addFoodTypeCancelButton = dialog.findViewById(R.id.addFoodTypeCancelButton);
+                                        addFoodTypeCancelButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                dialog.dismiss();
+                                            }
+                                        });
+
+                                        Button addFoodTypeCreateButton = dialog.findViewById(R.id.addFoodTypeCreateButton);
+                                        addFoodTypeCreateButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                // Elements
+                                                EditText addFoodNameEditText = dialog.findViewById(R.id.addFoodNameEditText);
+                                                EditText addFoodExpirationEditText = dialog.findViewById(R.id.addFoodExpirationEditText);
+                                                Spinner addFoodGroupSpinner = dialog.findViewById(R.id.addFoodGroupSpinner);
+
+                                                // Data
+                                                String name = addFoodNameEditText.getText().toString();
+                                                String expirationPeriodString = addFoodExpirationEditText.getText().toString().trim();
+                                                Enums.FoodGroup foodGroup = Enums.FoodGroup.values()[addFoodGroupSpinner.getSelectedItemPosition()];
+
+                                                boolean isValid = true;
+
+                                                // Ensure name field is filled in
+                                                if (name.isEmpty()) {
+                                                    addFoodNameEditText.setError("Please enter a name");
+                                                    isValid = false;
+                                                } else {
+                                                    addFoodNameEditText.setError(null);
+                                                }
+
+                                                // Ensure expiration date field is filled in
+                                                if (expirationPeriodString.isEmpty()) {
+                                                    addFoodExpirationEditText.setError("Please enter an expiration period");
+                                                    isValid = false;
+                                                } else {
+                                                    addFoodExpirationEditText.setError(null);
+                                                }
+
+                                                if (isValid) {
+                                                    int expirationPeriod = Integer.parseInt(expirationPeriodString);
+                                                    FoodType newFoodType = new FoodType(name, foodGroup, expirationPeriod);
+
+                                                    boolean exists = false;
+                                                    for (Map.Entry<Integer, FoodType> entry : foodDictionary.entrySet()) {
+                                                        FoodType existingFoodType = entry.getValue();
+                                                        if (existingFoodType.compareTo(newFoodType) == 0) {
+                                                            exists = true;
+                                                            addFoodNameEditText.setError("This item already exists");
+                                                            break;
+                                                        }
+                                                    }
+
+                                                    if (!exists) {
+                                                        Tab3.this.foodDictionary.put(newFoodType.getID(), newFoodType);
+                                                        Tab3.this.foodSelectorList.add(newFoodType);
+
+                                                        Tab3.this.foodSelectorListAdapter.notifyDataSetChanged();
+
+                                                        dialog.dismiss();
+                                                    }
+                                                }
+                                            }
+                                        });
+
+                                        dialog.show();
+                                    }
+                                });
+                            }
+                        });
+                /*
+                    ############################################
+                    ######## FOODTYPE SELECTOR CODE END ########
+                    ############################################
+                 */
+
+                        builder.setView(addDialogView);
+
+                        AlertDialog dialog = builder.create();
+                        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+                        // Set the dialog to occupy approximately 75% of the screen
+                        DisplayMetrics displayMetrics = new DisplayMetrics();
+                        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                        int width = (int) (displayMetrics.widthPixels * 0.75);
+                        int height = (int) (displayMetrics.heightPixels * 0.75);
+                        dialog.getWindow().setLayout(width, height);
+
+                        // Dim the background
+                        dialog.getWindow().setDimAmount(0.5f);
+
+                        cancelButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+                        addButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                boolean isValid = true;
+                                String countString = amount.getText().toString();
+
+                                if (selectedFood == null) {
+                                    foodTypeSelector.setError("Please select a food");
+                                    isValid = false;
+                                } else {
+                                    foodTypeSelector.setError(null);
+                                }
+
+                                if (countString.isEmpty()) {
+                                    amount.setError("Please enter a number of items");
+                                    isValid = false;
+                                } else {
+                                    amount.setError(null);
+                                }
+
+                                if (isValid) {
+                                    presetAddAdapter.addShopListItem(new ShopListItem(selectedFood, Integer.parseInt(amount.getText().toString())));
+                                    dialog.dismiss();
+                                }
+                            }
+                        });
+                        dialog.show();
+                    }
+                });
+
+
                 presetAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -405,7 +623,7 @@ public class Tab3 extends Fragment {
 
                         if (isValid) {
                             ArrayList<ShopListItem> temp = new ArrayList<ShopListItem>();
-                            for (ShopListItem i : shopList) {
+                            for (ShopListItem i : presetList) {
                                 temp.add(new ShopListItem(i.getFood(), i.getAmount()));
                             }
                             ShopListPresets.put(listName.getText().toString(), temp);
